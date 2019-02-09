22b2298d2901d4718541efe5898b694402cbe8fa
Kunal Kolhe <knl.kolhe@gmail.com>
2019-02-10 02:48:44 +0530

    Removed Gamma Correction. Added looping of manual Threshold. Added blur detection. Added rectangle bounding box. Crops image to keep only bounding box. Added Choosing best output string based on number of numeric characters in string. Not added yet: Eliminate random characters in detected string, Choose best string based on smarter parameters.

--- OCR/cardreader.py
+++ OCR/cardreader.py
@@ -6,7 +6,7 @@ Created on Thu Jan  3 02:08:32 2019
 """
 
 import pytesseract
-import imageio
+#import imageio
 import cv2
 import numpy as np
 
@@ -20,9 +20,19 @@ def Scan():
     cv2.namedWindow("test")
     
     img_counter = 0
-    
+    ret, frame = cam.read()
+    frame=cv2.flip(frame,1)
+    max_y,max_x=len(frame),len(frame[0])
+    #x1,y1=int(0.05*max_x),int(0.05*max_y)
+    P1=int(0.03*max_x),int(0.03*max_y)
+    P2=int(0.97*max_x),int(11/17*(0.97*max_x-0.03*max_x))
+    cv2.rectangle(frame,(P1),(P2),(0, 255, 0), 2)
+    cv2.imshow("test", frame)
     while True:
         ret, frame = cam.read()
+        frame=cv2.flip(frame,1)
+        max_y,max_x=len(frame),len(frame[0])
+        cv2.rectangle(frame,(P1),(P2),(0, 255, 0), 2)
         cv2.imshow("test", frame)
         if not ret:
             break
@@ -31,12 +41,16 @@ def Scan():
         if k%256 == 27:
             # ESC pressed
             print("Escape hit, closing...")
+            try:
+                img
+            except NameError: img=None
             break
         elif k%256 == 32:
             # SPACE pressed
             img_name = "I_{}.png".format(img_counter)
-            cv2.imwrite(img_name, frame)
-            img=frame
+            frame=cv2.flip(frame,1)
+            img=frame[P1[1]:P2[1],P1[0]:P2[0]]
+            cv2.imwrite(img_name, img)
             print("{} written!".format(img_name))
             img_counter += 1
     
@@ -78,88 +92,76 @@ def Gamma_correction(image) :
     else :
         return original
     
+def parse_1(Bstr):
+    card_no=''
+    for i in range(0,len(Bstr)):
+        if Bstr[i].isnumeric():
+            if len(Bstr)>i+19:
+                card_no=Bstr[i:i+19]
+                #if '\n' in card_no:
+                #    card_no=''
+                #else:
+                break
+            else:
+                card_no=Bstr[i:-1]
+    return card_no
+
+def parse_2(Bstr):
+    parts=Bstr.split('\n')
+    part=[]
+    for p in parts:
+        temp=p.split(' ')
+        for t in temp:
+            part.append(t)
+        
+    card_num=[]
+    for s in part:
+        if (s.isnumeric() and len(s)==4):
+            card_num.append(s)
+            
+    return card_num
+
+def variance_of_laplacian(image):
+	# compute the Laplacian of the image and then return the focus
+	# measure, which is simply the variance of the Laplacian
+	return cv2.Laplacian(image, cv2.CV_64F).var()
 #------------------------------------------------------------------------------
 pytesseract.pytesseract.tesseract_cmd = r'C:\!Kunal\Tesseract-OCR\tesseract.exe'
 
-
-
-#img=Scan()
-img = cv2.imread('I_00.png',-1)
+img=Scan()
+val=variance_of_laplacian(img)
+print("Blurry Variance:",val)
+blurthresh=600
+if val<blurthresh:
+    print("Image is blurry, Please Try Again.")
+    img=None
+#img = cv2.imread('I_1.png',-1)
 #img=cv2.resize(img,(800,500))
-
-display(img)
-img=Gamma_correction(img)
-display(img)
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-'''gaussian thresholding'''
-(thresh, im_gauss) = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-#img = cv2.medianBlur(img, 3)
-'''manual thresholding'''
-#Bigger number more black, Smaller number more white. Start at 70 till you get a good image
-thresh = 70
-im_bw = cv2.threshold(img_gray, thresh, 255, cv2.THRESH_BINARY)[1]
-
-display(im_bw)
-
-outputs=[]
-outputs.append(pytesseract.image_to_string(img,lang="eng"))
-print("String 1, Image normal: ",outputs[-1])
-
-outputs.append(pytesseract.image_to_string(img_gray,lang="eng"))
-print("String 2, Image gray: ",outputs[-1])
-
-outputs.append(pytesseract.image_to_string(im_gauss,lang="eng"))
-print("String 3, Gaussian Threshold: ",outputs[-1])
-
-outputs.append(pytesseract.image_to_string(im_bw,lang="eng"))
-print("String 4, Manual Threshold: ",outputs[-1])
-'''
-card=r'[0-9][0-9][0-9][0-9]'
-
-result=re.match(card,str4,re.I)
-print(result.group())
-'''
-#import string
-
-strlen=[]
-for op in outputs:
-    strlen.append(len(op))
-
-
-
-idx=getMaxIdx(strlen)
-Bstr=outputs[idx]
-
-card_no=''
-for i in range(0,len(Bstr)):
-    if Bstr[i].isnumeric():
-        if len(Bstr)>i+19:
-            card_no=Bstr[i:i+19]
-            #if '\n' in card_no:
-            #    card_no=''
-            #else:
-            break
-        else:
-            card_no=Bstr[i:-1]
-        
-        
-print("Parsed Card Number (Simple): ",card_no)
-
-parts=Bstr.split('\n')
-part=[]
-for p in parts:
-    temp=p.split(' ')
-    for t in temp:
-        part.append(t)
+if img is None:
+    print("Image not captured/not captured properly. Press Spacebar when window is open to capture image.")
+else:
+    display(img)
+    #img=Gamma_correction(img)
+    #display(img)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-card_num=[]
-for s in part:
-    if (s.isnumeric() and len(s)==4):
-        card_num.append(s)
-
-print("Method 2: ",card_num)
-        
-
-
+    outputs=[]
+    '''manual thresholding'''
+    #Bigger number more black, Smaller number more white. Start at 70 till you get a good image
+    for thresh in range(70,90,5):
+        im_bw = cv2.threshold(img_gray, thresh, 255, cv2.THRESH_BINARY)[1]
+        #display(im_bw)
+        outputs.append(pytesseract.image_to_string(im_bw,lang="eng"))
+        print("----String, Manual Threshold: ",outputs[-1])
+    
+    strlen=[]
+    for op in outputs:
+        strlen.append(sum(c.isdigit() for c in op))
+    
+    Bstr=outputs[getMaxIdx(strlen)]
+    print("Best String Found is:",Bstr) 
+    card_no=parse_1(Bstr)
+    print("Parsed Card Number (Simple): ",card_no)
+    
+    card_num=parse_2(Bstr)
+    print("Method 2: ",card_num)
